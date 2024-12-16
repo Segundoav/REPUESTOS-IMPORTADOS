13323943f7f0a1cefa58bcfe5de0a2f877bfe6e9
segundoav <segundoav@hotmail.com>
2024-12-16 00:31:22 -0500

    primeros cambios de mi paguina nueva d eimportaciones

---
+++ script.js
@@ -0,0 +1,29 @@
+function mostrarCategoria(idCategoria) {
+    // Obtener el elemento de la categoría seleccionada
+    let categoriaSeleccionada = document.getElementById(idCategoria);
+    
+    // Hacer scroll hacia la categoría seleccionada
+    categoriaSeleccionada.scrollIntoView({ behavior: 'smooth' });
+}
+
+function mostrarDetalle(nombre, precio, imagen) {
+    // Mostrar el detalle del producto
+    document.getElementById('nombreProducto').innerText = nombre;
+    document.getElementById('precioProducto').innerText = precio;
+    document.getElementById('imagenDetalle').src = imagen;
+
+    // Mostrar el cuadro de detalle
+    document.getElementById('detalleProducto').style.display = 'flex';
+}
+
+function cerrarDetalle() {
+    // Ocultar el cuadro de detalle
+    document.getElementById('detalleProducto').style.display = 'none';
+}
+
+
+
+
+
+
+
